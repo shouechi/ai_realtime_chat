1ded7f2f1913a348b17b75aadccc7198b31a4bb0
shouechi <hajimete.1105@gmail.com>
2025-11-08 22:09:42 +0900

    add: 最小UIとエコー送信

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -1,5 +1,14 @@
 import consumer from "channels/consumer"
 
+let subscription
+export function subscribeChat(conversationId, onMessage) {
+  if (subscription) subscription.unsubscribe()
+  subscription = consumer.subscriptions.create(
+    { channel: "ChatChannel", conversation_id: conversationId || "global" },
+    { received(data) { onMessage?.(data) } }
+  )
+}
+
 consumer.subscriptions.create("ChatChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "messages#index"
+  resources :messages, only: [:index, :create]
   mount ActionCable.server => "/cable"
 end

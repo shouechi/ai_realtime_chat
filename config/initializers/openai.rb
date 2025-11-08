92eb6e086d59b2ae962bf66a3bce910e4d9c8c57
shouechi <hajimete.1105@gmail.com>
2025-11-08 22:42:32 +0900

    fix: API関連の修正

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,3 +1,4 @@
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.openai[:api_key]
+  config.access_token = ENV["OPENAI_API_KEY"] ||
+                        Rails.application.credentials.dig(:openai, :api_key)
 end
